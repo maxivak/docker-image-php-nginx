22411e0d276ab23c2cbacd3a3ef1f1876f71d2f8
Max Ivak <max.ivak@galacticexchange.io>
2017-01-18 13:19:52 +0200

    packages

--- packages.sh
+++ packages.sh
@@ -6,9 +6,14 @@ add-apt-repository ppa:ondrej/php
 
 apt-get update
 
+# some base packages
+apt-get --no-install-recommends --allow-unauthenticated -y install iproute2
+
+# php
 #apt-get --no-install-recommends -y install nginx php5-fpm php5-mysql php5-imagick php5-mcrypt php5-curl php5-cli php5-memcache php5-intl php5-gd php5-xdebug curl
 apt-get --no-install-recommends --allow-unauthenticated -y install nginx
 apt-get --no-install-recommends --allow-unauthenticated -y install php5.6 php5.6-mysql php5.6-mbstring php5.6-curl php5.6-gd php5.6-mcrypt php5.6-xml php5.6-xmlrpc php5.6-fpm
+
 
 # Cleanup
 apt-get clean
